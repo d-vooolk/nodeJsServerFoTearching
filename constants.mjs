6d3ae0cfaca34c53baa0c12246b5290bf33c4d38
d-vooolk <volk.dmitrii@otr.ru>
2023-07-22 02:35:41 +0300

    add modularity

---
+++ constants.mjs
@@ -0,0 +1,6 @@
+export const profileData = {
+    id: 1,
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    age: 30,
+};
--- server.mjs
+++ server.mjs
@@ -1,14 +1,9 @@
-const express = require('express');
-const cors = require('cors');
+import { profileData } from "./constants.mjs";
+import express from 'express';
+import cors from 'cors';
+
 const app = express();
 const port = 3000;
-
-const profileData = {
-    id: 1,
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-    age: 30,
-};
 
 app.use(cors());
 
