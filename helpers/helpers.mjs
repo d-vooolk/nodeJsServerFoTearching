1575ca9fe837af4d22f128c3a25bf1413f3a7638
d-vooolk <volk.dmitrii@otr.ru>
2023-08-02 15:23:38 +0300

    replace function in file

--- helpers/helpers.mjs
+++ helpers/helpers.mjs
@@ -9,6 +9,25 @@ export const dbConnect = (path) => new sqlite3.Database(path, (err) => {
     }
 });
 
+export const generateRandomUser = () => {
+    const personSex = faker.person.sex();
+    return {
+        avatar: faker.image.avatar(),
+        gender: personSex,
+        age: faker.number.int({ min: 18, max: 65 }),
+        name: faker.person.firstName(personSex),
+        secondName: faker.person.lastName(personSex),
+        aboutMe: faker.lorem.sentence(),
+        status: faker.helpers.arrayElement(['single', 'married', 'divorced']),
+        city: faker.location.city(),
+        phoneNumber: faker.phone.number('+375 29 ### ## ##'),
+        telegram: '@' + faker.internet.userName(),
+        instagram: faker.internet.userName(),
+        twitter: faker.internet.userName(),
+        friends: [],
+        subscribers: [],
+    };
+}
 
 export function parseJSONFields(data) {
     for (const key in data) {
@@ -40,26 +59,6 @@ export const convertPropertiesToJson = (obj) => {
 export const catchError = (error, res) => {
     console.error('Ошибка при обновлении данных:', error.message);
     res.status(500).json({ error: 'Ошибка при обновлении данных' });
-}
-
-export const generateRandomUser = () => {
-    const personSex = faker.person.sex();
-    return {
-        avatar: faker.image.avatar(),
-        gender: personSex,
-        age: faker.number.int({ min: 18, max: 65 }),
-        name: faker.person.firstName(personSex),
-        secondName: faker.person.lastName(personSex),
-        aboutMe: faker.lorem.sentence(),
-        status: faker.helpers.arrayElement(['single', 'married', 'divorced']),
-        city: faker.location.city(),
-        phoneNumber: faker.phone.number('+375 29 ### ## ##'),
-        telegram: '@' + faker.internet.userName(),
-        instagram: faker.internet.userName(),
-        twitter: faker.internet.userName(),
-        friends: [],
-        subscribers: [],
-    };
 }
 
 
